daa6a13d456ae1d72667ecd26d266ac885900163
alvezandreluiz <alvezandreluiz@gmail.com>
2024-10-11 21:29:29 -0300

    content and routes creation

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photoCover":"/biribiri.jpg",
+        "title":"Biribiri State Park",
+        "titleLink":"https://maps.app.goo.gl/LiEXPtqffvzbUidr8",
+        "description":"Biribiri State Park was created in 1998 and is located 13km from the city of Diamantina in the state of Minas Gerais - Brazil. The main attractions are waterfalls, natural wells and the village of Biribiri."
+    },
+    {
+        "id":"2",
+        "photoCover":"/peruacu.jpg",
+        "title":"Peruaçu Caves National Park",
+        "titleLink":"https://maps.app.goo.gl/i3hDUKt6F2wx362s6",
+        "description":"The Peruaçu Caves National Park is a conservation unit created by decree on September 21, 1999, whose main objective is to protect the geological and archaeological heritage found in the northern region of Minas Gerais. It is home to over 140 caves, more than 80 archaeological sites and cave paintings, as well as the indigenous Xakriabás tribe."
+    },
+    {
+        "id":"3",
+        "photoCover":"/rio-preto.jpg",
+        "title":"Rio Preto State Park",
+        "titleLink":"https://maps.app.goo.gl/BBRLNXqVXYSLwQwR8",
+        "description":"The Rio Preto State Park, created on September 27, 1993, is located in the city of São Gonçalo do Rio Preto, in the Espinhaço mountain range complex, Minas Gerais, Brazil. The park is open to visitors with several trails. It is also home to ground zero of the Royal Road and is part of the Diamond Tourist Circuit."
+    },
+    {
+        "id":"4",
+        "photoCover":"/serra-do-cipo.jpg",
+        "title":"Serra do Cipó National Park",
+        "titleLink":"https://maps.app.goo.gl/Bvc5rpWLGh9mBHJR8",
+        "description":"The Serra do Cipó National Park is a Brazilian conservation unit located in the Espinhaço mountain range in Minas Gerais. Its geological history is complex and dates back to the Precambrian period, with its sandy rocks having been formed by marine deposits over 1.7 billion years ago. There are a total of 100,000 hectares of savannahs, rupestrian fields and woods, as well as rivers, waterfalls, canyons, caves, preserved archaeological sites and many adventure sports."
+    }
+]
